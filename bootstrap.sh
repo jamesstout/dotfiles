a9d18389763211051d98077f3c97ac1cc1beeb62
jamesstout <stoutyhk@gmail.com>
2013-02-15 02:19:14 +0800

    not wanted

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
-#git pull
-#function doIt() {
-#	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-#}
-#if [ "$1" == "--force" -o "$1" == "-f" ]; then
-#	doIt
-#else
-#	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-#	echo
-#	if [[ $REPLY =~ ^[Yy]$ ]]; then
-#		doIt
-#	fi
-#fi
-#unset doIt
+
+cp .{bash_profile,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc} ~
+
 source ~/.bash_profile
