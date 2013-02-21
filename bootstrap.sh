c34e045374b6c70adbdef69e1ec041d1d48c8756
jamesstout <stoutyhk@gmail.com>
2013-02-21 23:39:20 +0800

    Brew utils etc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 
-cp .{bash_profile,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc} ~
+source ./.brew
+
+cp .{bash_profile,bash_prompt,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc} ~
 
 source ~/.bash_profile
