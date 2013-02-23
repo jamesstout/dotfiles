44e353328d41f0023c664d5940f6541286b27594
jamesstout <stoutyhk@gmail.com>
2013-02-23 20:14:23 +0800

    backup and copy .vim dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 
+# backup
 cp ~/.{bash_profile,bash_prompt,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc} ~/.backups
+cp -r ~/.vim ~/.backups
 
 source ./.brew
 
+cp -rf .vim ~
 cp .{bash_profile,bash_prompt,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc} ~
 
 source ~/.bash_profile
