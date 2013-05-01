e46d3707689aff4eaae60e9bd6b90a60ec0bb964
jamesstout <stoutyhk@gmail.com>
2013-05-02 03:30:27 +0800

    extra dir checks, symlink checks and z update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,71 @@
-#!/bin/bash
+#!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 
+BACKUPS_DIR="$HOME"/.backups
+
+if [ ! -d "$BACKUPS_DIR" ]; then
+	printf "$(tput setaf 136)! %s$(tput sgr0)\n" "$BACKUPS_DIR does not exist, creating..."
+	mkdir "$BACKUPS_DIR" || printf "$(tput setaf 1)x %s$(tput sgr0)\n" "Could not create $BACKUPS_DIR" ; printf "Aborting...\n"; return 1
+fi
+
 # backup
-cp ~/.{bash_profile,bash_prompt,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc,utils,bashrc} ~/.backups
-cp -r ~/.vim ~/.backups
+# backup .z - it contains all the z directory info, just in case
+cp ~/.{bash_profile,bash_prompt,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc,utils,bashrc,z} "$BACKUPS_DIR"
+cp -r ~/.vim "$BACKUPS_DIR"
 
-source ./.brew
-
+# update dotfiles
 cp -rf .vim ~
 cp .{bash_profile,bash_prompt,path,exports,aliases,functions,extra,gitattributes,gitconfig,gitignore,inputrc,hgignore,wgetrc,vimrc,utils,bashrc} ~
+
+# move down here, depends on .utils
+source ./.brew
+
+# setup vars for dirs and symlinks
+BIN_DIR="$HOME"/bin
+SUBL_SYMLINK="$BIN_DIR"/subl
+SUBL2_APP="/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+SUBL3_APP="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+# default to v2
+SUBL_APP="$SUBL2_APP"
+Z_REPO="third-party/z"
+
+if file_exists "$SUBL3_APP"; then
+	SUBL_APP="$SUBL3_APP"
+fi
+
+# check on bin and links
+# should exist but if not...
+if [ ! -d "$BIN_DIR" ]; then
+	e_warning "$BIN_DIR does not exist, creating..."
+	if mkdir "$BIN_DIR"; then
+		printf "Creating subl symlink"
+		ln -s "$SUBL_APP" "$SUBL_SYMLINK"
+
+		# update z repo and copy
+		cd "$Z_REPO"
+		git pull
+		cp -f z.sh "$BIN_DIR"
+		chmod +x "$BIN_DIR"/z.sh
+		cd -
+	else
+		e_error "Could not create $BIN_DIR" ; 
+		e_warning "$(tput setaf 136)! %s$(tput sgr0)\n" "Sublime Text symlink and z will not be installed";
+	fi
+else
+
+	# check subl symlink exists if not create it
+	if ! file_exists "$SUBL_SYMLINK"; then
+		printf "Creating subl symlink"
+		ln -s "$SUBL_APP" "$SUBL_SYMLINK"
+	fi
+
+	# update z repo and copy
+	cd "$Z_REPO"
+	git pull
+	cp -f z.sh "$BIN_DIR"
+	chmod +x "$BIN_DIR"/z.sh
+	cd -
+
+fi
 
 source ~/.bash_profile
