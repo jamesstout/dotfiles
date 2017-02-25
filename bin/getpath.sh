a888ef69a372b5246d1a75bd62fde03805bf539b
James Stout <stoutyhk@gmail.com>
2017-02-25 16:21:43 +0800

    new scripts - de-dupe eternal history and get path of app

---
+++ bin/getpath.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Array of possible Firefox application names.
+appname="HKBNLocalSpeedTest"    # "Firefox" "IceWeasel" "etc
+
+#
+# Calls lsregister -dump and parses the output for "/Firefox.app", etc.
+# Returns the very first result found.
+#
+function get_osx_ffdir()
+{
+    # OSX Array of possible lsregister command locations
+    # I'm only aware of this one currently
+    lsregs=("/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister")
+
+    for i in "${lsregs[@]}"; do
+            if [ -f $i ]; then
+                echo "$i"
+                # Some logic to parse the output from lsregister
+                # pathArray=($($i -dump |grep -E "/$appname.app$" |cut -d'/' -f2-))
+                pathArray=("Volumes/HKBNLocalSpeedTest/HKBNLocalSpeedTest.app" "Volumes/Hard Drive 2/james/Downloads/HKBNLocalSpeedTest.app")
+
+                # ffdir=$($i -dump |grep -E "/$appname.app$" |cut -d'/' -f2- |head -1)
+                for path in "${pathArray[@]}"
+                do
+                    echo "$path"
+                    ffdir="/$path"
+
+                    if [[ "$path" =~ ^Application* ]]; then
+                        echo "in regex, breaking"
+                        ffdir="/$path"
+                    fi
+                    echo "ffdir = $ffdir"
+                done
+
+                return 0
+            fi
+    done
+    return 1
+}
+
+#
+# Uses "which" and "readlink" to locate firefox on Linux, etc
+#
+
+
+
+echo "Searching for Firefox..."
+
+ffdir=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Mac OSX
+    get_osx_ffdir
+else
+    # Linux, etc
+    get_ffdir
+fi
+
+echo "Found application here: $ffdir"
+
