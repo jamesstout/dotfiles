0f42f3c1c209ecff8412e804c975f8a31e8c2b2e
James Stout <stoutyhk@gmail.com>
2018-01-21 03:18:28 +0800

    WIP

--- bin/de-dupe-bash-eternal-history.sh
+++ bin/de-dupe-bash-eternal-history.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 cp .bash_eternal_history .bash_eternal_history.bak3
-cp .bash_eternal_history.bak .bash_eternal_history
+cp .bash_eternal_history .bash_eternal_history.bak
+#cp .bash_eternal_history.bak .bash_eternal_history
 
-sort .bash_eternal_history.bak |grep '^[0-9]' |  sort -k2 -u > new6
-sort .bash_eternal_history.bak |grep '^[0-9]' | sed 's/ *$//' | sort -k2 -u > new7
+sort .bash_eternal_history.bak4 |grep '^[0-9]' |  sort -k2 -u > new6
+sort .bash_eternal_history.bak4 |grep '^[0-9]' | sed 's/ *$//' | sort -k2 -u > new7
 
-sort .bash_eternal_history.bak |grep '^[0-9]' | sed 's/ *$//' | sort -k2 -u > .bash_eternal_history
+sort .bash_eternal_history.bak4 |grep '^[0-9]' | sed 's/ *$//' | sort -k2 -u > .bash_eternal_history
